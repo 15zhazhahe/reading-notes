0fa0eb1412056630edd263075bef3f9eb341783d
15zhazhahe <729260486@qq.com>
2018-04-15 10:16:24 +0800

    insert data

--- SQL-Basic-tutorials/Chapter01.sql
+++ SQL-Basic-tutorials/Chapter01.sql
@@ -74,3 +74,20 @@ SELECT * FROM shop.Jyushoroku;
 
 -- 1.3
 DROP TABLE shop.Jyushoroku;
+
+-- DML 插入数据
+START TRANSACTION;
+
+INSERT INTO shop.Shopin VALUES ('0001', 'T恤衫', '衣服', 1000, 500, '2009-09-20');
+INSERT INTO shop.Shopin VALUES ('0002', '打孔器', '办公用品', 500, 320, '2009-09-11');
+INSERT INTO shop.Shopin VALUES ('0003', '运动T恤', '衣服', 4000, 2800, NULL);
+INSERT INTO shop.Shopin VALUES ('0004', '菜刀', '厨房用具', 3000, 2800, '2009-09-20');
+INSERT INTO shop.Shopin VALUES ('0005', '高压锅', '厨房用具', 6800, 5000, '2009-01-15');
+INSERT INTO shop.Shopin VALUES ('0006', '叉子', '厨房用具', 500, NULL, '2009-09-20');
+INSERT INTO shop.Shopin VALUES ('0007', '擦菜板', '厨房用具', 880, 790, '2008-04-28');
+INSERT INTO shop.Shopin VALUES ('0008', '圆珠笔', '办公用品', 100, NULL, '2009-11-11');
+
+COMMIT;
+
+SELECT * 
+FROM shop.Shopin;
